50e49889190c8532412587413cf67f22838b343d
Adonis Hernandez <kaok12hernandez@gmail.com>
2024-09-13 10:37:43 -0600

    All Updated

---
+++ src/blackjack/usecases/pedir-cartas.js
@@ -0,0 +1,16 @@
+
+/**
+ * Esta funcion pide una carta.
+ * @param {Arrays<String>} deck es un arreglo de string.
+ * @returns  {String} Retorna una carta del deck.
+ */
+
+export const pedirCarta = ( deck ) => {
+
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
